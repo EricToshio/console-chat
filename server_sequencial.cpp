4fc667b104c17245841bea002d6ea6fbbeb20f7e
Eric Toshio <eric.toshio.e.s@gmail.com>
2019-04-15 13:13:50 -0300

    codigo sequencial

---
+++ server_sequencial.cpp
@@ -0,0 +1,98 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <time.h>
+
+
+// -----------------------------------------
+// Configuracoes
+// -----------------------------------------
+
+// Configiracao do teste
+#define NUMBER_OF_CLIENTS 2
+#define NUMBER_OF_MESSAGES_FOR_CLIENT 100
+
+int messagesSent[NUMBER_OF_CLIENTS];
+int index_available;
+
+// -----------------------------------------
+// lista ligada de mensagens 
+// -----------------------------------------
+int created_blocks;
+struct noh {
+	int id;
+	std::string message;
+	struct noh* next;
+} head;
+
+struct noh* last;
+
+struct noh* add_noh(int id, std::string message, struct noh* last_noh) {
+	struct noh* new_noh = new noh;
+	new_noh->next = NULL;
+	new_noh->id = id;
+	new_noh->message = message;
+	if(last_noh != last){
+		printf("<<<<<<<<<<<<<>>>>>>>>>>>>\n");
+		printf("usuario %d usou uma versao desatualizada da lista\n",id);
+	}
+	last_noh->next = new_noh;
+	last_noh = new_noh;
+	created_blocks++;
+	return last_noh;
+}
+// -----------------------------------------
+// Classe servidor
+// -----------------------------------------
+class Server
+{
+public:
+	
+	Server();
+	static void client_send_message(int index);
+	
+};
+// -----------------------------------------
+// MAIN
+// -----------------------------------------
+int main(int argc, char const *argv[])
+{
+	clock_t t;
+	t = clock();
+	Server server;
+	t = clock() - t;
+	std::cout << "Demorou " << ((float)t)/CLOCKS_PER_SEC << " segundos" << std::endl;
+	return 0;
+}
+// -----------------------------------------
+// Funcoes da classe servidor
+// -----------------------------------------
+// Construtor
+Server::Server()
+{	
+	index_available = 0;
+	created_blocks = 0;
+	head.next = NULL;
+	last = &head;
+	
+
+	for (int i = 0; i < NUMBER_OF_MESSAGES_FOR_CLIENT; ++i){
+		for(int j = 0; j<NUMBER_OF_CLIENTS; ++j) {
+			client_send_message(j);
+		}
+	}
+
+}
+
+void Server::client_send_message(int index) {
+
+	//std::cout << "Usuario " << id << " conectado com indice " << index << std::endl;
+	std::string message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor eu erat ut porta. In feugiat turpis auctor elit laoreet accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus.";
+	sleep(1);
+	last = add_noh(index, message, last);
+}
+
+
